4c760eb4e460f87e79d18c72edf86b2afe7e125a
下村芽生 <mei.shimomura.hal@gmail.com>
2022-01-14 10:24:41 +0900

    Homeコンポーネント仮を定義

---
+++ src/features/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import App from "../../App";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <App>
+        <div>あああ</div>
+      </App>
+    </div>
+  );
+};
+
+export default Home;

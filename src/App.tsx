fe0d9d109a076b75d788becfb0fddfb7465d1c20
下村芽生 <mei.shimomura.hal@gmail.com>
2022-01-14 10:19:20 +0900

    Appコンポーネントの変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
-import { Auth } from "features/auth/Auth";
+import DrawerHeader from "./features/common/DrawerHeader";
 import "./App.css";
 
-function App() {
+const App: React.FC = ({ children }) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Auth />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <DrawerHeader>{children}</DrawerHeader>
     </div>
   );
-}
+};
 
 export default App;

f4fe846f25e0050a3fd00dd1298732598a2cf4bf
下村芽生 <mei.shimomura.hal@gmail.com>
2022-01-21 09:34:59 +0900

    index.tsxを変更

--- src/index.tsx
+++ src/index.tsx
@@ -5,34 +5,11 @@ import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
-import { Route, BrowserRouter } from "react-router-dom";
-
-import Auth from "features/auth/Auth";
-import Home from "features/home/Home";
-
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#0679EB",
-    },
-  },
-});
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <CssBaseline />
-        <ThemeProvider theme={theme}>
-          <div>
-            <Route exact path="/signin" component={Auth} />
-            <Route exact path="/home" component={Home} />
-          </div>
-        </ThemeProvider>
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")

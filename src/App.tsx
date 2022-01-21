ad01f609fdb47592397f36c7cb6b3fd3c4351f85
下村芽生 <mei.shimomura.hal@gmail.com>
2022-01-21 09:32:10 +0900

    Appコンポーネントを変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import React from "react";
 import logo from "./logo.svg";
-import DrawerHeader from "./features/common/DrawerHeader";
 import "./App.css";
+import { useSelector } from "react-redux";
 
-const App: React.FC = ({ children }) => {
+import { Route, BrowserRouter, Redirect } from "react-router-dom";
+
+import Auth from "features/auth/Auth";
+import Home from "features/home/Home";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { StyledEngineProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0679EB",
+    },
+  },
+});
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <DrawerHeader>{children}</DrawerHeader>
-    </div>
+    <BrowserRouter>
+      <StyledEngineProvider injectFirst>
+        <CssBaseline />
+        <ThemeProvider theme={theme}>
+          <div>
+            <Route exact path="/signin" component={Auth} />
+            <Route exact path="/home" component={Home} />
+          </div>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </BrowserRouter>
   );
 };
 

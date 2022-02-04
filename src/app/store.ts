d5d6d7155478d067074c83e4f2e933e9fba5f7dd
下村芽生 <mei.shimomura.hal@gmail.com>
2022-02-04 10:58:46 +0900

    storeに追加

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,18 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import authReducer from "features/auth/authSlice";
 import commonReducer from "features/common/commonSlice";
+import notifySelfReducer from "features/notifySelf/notifySelfSlice";
+import notificationReducer from "features/notification/notificationSlice";
+import commentReducer from "features/comment/commentSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     counter: counterReducer,
     common: commonReducer,
+    notifySelf: notifySelfReducer,
+    notification: notificationReducer,
+    comment: commentReducer,
   },
 });
 
